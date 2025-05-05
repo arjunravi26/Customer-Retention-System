0babbb8c1bae46225a4f2aa000c3cbfaef9c5919
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-05-05 10:04:23 +0530

    connnect with topic modeling api

---
+++ website/top2vec_model.py
@@ -0,0 +1,53 @@
+import requests
+import json
+from .data import fetch_data
+from .logger import logging
+def send_documents(documents):
+    """Send documents to the API for topic modeling."""
+    url = "http://localhost:8005/process"
+    payload = {"documents": documents}
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        logging.info(f"Send documnet {payload}")
+        print("Process Response:", response.json())
+    except requests.RequestException as e:
+        logging.info(f"Error in sending documents: {str(e)}")
+        print(f"Error sending documents: {str(e)}")
+
+def receive_topics():
+    """Retrieve topics from the API."""
+    url = "http://localhost:8005/topics"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        topics = response.json()
+        print("Process Response:", response.json())
+        print("Topics:")
+        logging.info("Topics: ")
+
+        for topic in topics:
+            print(f"- {topic['topic_name']}: {topic['description']} (Frequency: {topic['frequency']}")
+            logging.info(f"- {topic['topic_name']}: {topic['description']} (Frequency: {topic['frequency']})")
+        return topics[:8]
+    except requests.RequestException as e:
+        logging.info(f"Error retrieving topics: {str(e)}")
+        print(f"Error retrieving topics: {str(e)}")
+        return []
+
+
+# if __name__ == "__main__":
+#     # Sample documents (replace with your data)
+
+#     documents = fetch_data()
+#     # Send documents for processing
+#     send_documents(documents)
+
+#     # Retrieve and print topics
+#     topics = get_topics()
+#     print(topics)
+
+#     # Retrieve documents for the first topic (if available)
+#     if topics:
+#         first_topic_id = topics[0]["topic_id"]
+#         print(get_documents_by_topic(first_topic_id))
